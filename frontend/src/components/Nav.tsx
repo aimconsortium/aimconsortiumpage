dfe82890fdea494f0a409a6abf2c5f64e94ad2a0
josfam <jamukun@proton.me>
2026-04-24 21:28:07 +0300

    Add motion to dropdown

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -16,21 +16,75 @@ import { animationPresets } from "../constants/animationPresets";
 
 export const Nav = () => {
   const shouldReduceMotion = useReducedMotion();
-  const largePreset = animationPresets.large;
+  const mediumPreset = animationPresets.medium;
+  const extraLargePreset = animationPresets.extraLarge;
   const [isDropDownOpen, setIsDropdownOpen] = useState(false); // dropdown behavior for mobile
+
+  // ref for the dropdown
+  const dropDownRef = useRef<HTMLElement>(null) as React.RefObject<HTMLElement>;
+  const handleClickOutsideDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+  useOnClickOutside(dropDownRef, handleClickOutsideDropdown);
 
   return (
     <header>
       {/* desktop */}
-      <nav className="fixed left-1/2 z-50 mt-(--nav-top-gap) hidden h-(--nav-height) -translate-x-1/2 items-center justify-center gap-4 rounded-(--nav-rounding) bg-(--primary-50) p-(--nav-padding) shadow-xl outline outline-(--primary-200) backdrop-blur-3xl sm:flex">
+      <motion.nav
+        className="fixed left-1/2 z-50 mt-(--nav-top-gap) hidden h-(--nav-height) -translate-x-1/2 items-center justify-center gap-4 rounded-(--nav-rounding) bg-(--primary-50) p-(--nav-padding) shadow-xl outline outline-(--primary-200) backdrop-blur-3xl sm:flex"
+        initial={
+          shouldReduceMotion
+            ? {}
+            : {
+                y: -mediumPreset.distance,
+                opacity: 0,
+                filter: `blur(${mediumPreset.blurAmount}px)`,
+              }
+        }
+        animate={
+          shouldReduceMotion ? {} : { y: 0, opacity: 1, filter: "blur(0px)" }
+        }
+        transition={
+          shouldReduceMotion
+            ? {}
+            : {
+                duration: mediumPreset.duration,
+                delay: mediumPreset.delay,
+                ease: easeInOut,
+              }
+        }
+      >
         <HeaderLink text="Home" to="/" />
         <HeaderLink text="Team" to="/team" />
         <HeaderLink text="Portfolio" to="/portfolio" />
         <HeaderLink text="Contact" to="/contact" />
-      </nav>
+      </motion.nav>
 
       {/* mobile */}
-      <nav className="fixed left-1/2 z-50 mt-(--nav-top-gap) flex h-(--nav-height) w-[calc(90%-1.5rem)] -translate-x-1/2 items-center justify-center gap-4 rounded-(--nav-rounding) bg-(--primary-50) p-(--nav-padding) shadow-xl outline outline-(--primary-200) backdrop-blur-3xl sm:hidden">
+      <motion.nav
+        className="fixed left-1/2 z-50 mt-(--nav-top-gap) flex h-(--nav-height) w-[calc(90%-1.5rem)] -translate-x-1/2 items-center justify-center gap-4 rounded-(--nav-rounding) bg-(--primary-50) p-(--nav-padding) shadow-xl outline outline-(--primary-200) backdrop-blur-3xl sm:hidden"
+        initial={
+          shouldReduceMotion
+            ? {}
+            : {
+                y: -mediumPreset.distance,
+                opacity: 0,
+                filter: `blur(${mediumPreset.blurAmount}px)`,
+              }
+        }
+        animate={
+          shouldReduceMotion ? {} : { y: 0, opacity: 1, filter: "blur(0px)" }
+        }
+        transition={
+          shouldReduceMotion
+            ? {}
+            : {
+                duration: mediumPreset.duration,
+                delay: mediumPreset.delay,
+                ease: easeInOut,
+              }
+        }
+      >
         <div className="relative flex w-full items-center justify-between">
           <Link to="/">
             <img
@@ -39,38 +93,89 @@ export const Nav = () => {
               className="ml-(--nav-mobile-spacing-nudge) h-8 w-8 object-cover transition-transform duration-200 hover:scale-110"
             />
           </Link>
-          <button>
+          <button className="group relative flex items-center justify-center">
             {isDropDownOpen ? (
-              <RxCross2
-                className="mr-(--nav-mobile-spacing-nudge) h-8 w-8 cursor-pointer text-(--primary-900)"
-                onClick={() => setIsDropdownOpen(false)}
-              />
+              <div className="group relative mr-(--nav-mobile-spacing-nudge) flex items-center justify-center">
+                <RxCross2
+                  className="z-40 h-8 w-8 cursor-pointer text-(--primary-900)"
+                  onClick={() => setIsDropdownOpen(false)}
+                />
+                <span
+                  className="pointer-events-none absolute z-30 h-15 w-15 cursor-pointer rounded-full from-(--primary-100) from-30% to-(--primary-200) group-hover:bg-linear-to-b"
+                  onClick={() => setIsDropdownOpen(true)}
+                ></span>
+              </div>
             ) : (
-              <RxHamburgerMenu
-                className="mr-(--nav-mobile-spacing-nudge) h-8 w-8 cursor-pointer text-(--primary-900)"
-                onClick={() => setIsDropdownOpen(true)}
-              />
+              <div className="group relative mr-(--nav-mobile-spacing-nudge) flex items-center justify-center">
+                <RxHamburgerMenu
+                  className="z-40 h-8 w-8 cursor-pointer text-(--primary-900)"
+                  onClick={() => setIsDropdownOpen(true)}
+                />
+                <span
+                  className="pointer-events-none absolute z-30 h-15 w-15 cursor-pointer rounded-full from-(--primary-100) from-30% to-(--primary-200) group-hover:bg-linear-to-b"
+                  onClick={() => setIsDropdownOpen(true)}
+                ></span>
+              </div>
             )}
           </button>
         </div>
-      </nav>
+      </motion.nav>
       {/* dropdown menu */}
-      <div
-        className={`fixed left-1/2 z-40 mt-(--nav-top-gap) flex h-fit w-[calc(90%-1.5rem)] -translate-x-1/2 flex-col gap-y-3 rounded-(--nav-rounding) bg-(--primary-50)/50 px-(--nav-padding) pt-[calc(var(--nav-height)*1.3)] pb-(--nav-padding) shadow-xl outline outline-(--primary-200) ${isDropDownOpen ? "block" : "hidden"} backdrop-blur-xl sm:hidden`}
-      >
-        <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
-          <HeaderLink text="Home" to="/" />
-        </div>
-        <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
-          <HeaderLink text="Team" to="/team" />
-        </div>
-        <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
-          <HeaderLink text="Portfolio" to="/portfolio" />
-        </div>
-        <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
-          <HeaderLink text="Contact" to="/contact" />
-        </div>
-      </div>
+      <AnimatePresence>
+        <motion.div
+          ref={dropDownRef}
+          className={`fixed left-1/2 z-40 mt-(--nav-top-gap) flex h-fit w-[calc(90%-1.5rem)] -translate-x-1/2 flex-col gap-y-3 rounded-(--nav-rounding) bg-(--primary-50)/50 px-(--nav-mobile-spacing-nudge) pt-[calc(var(--nav-height)*1.3)] pb-(--nav-padding) shadow-xl outline outline-(--primary-200) ${isDropDownOpen ? "block" : "hidden"} backdrop-blur-xl sm:hidden`}
+          initial={
+            shouldReduceMotion
+              ? {}
+              : {
+                  y: -mediumPreset.distance,
+                  scale: mediumPreset.scaleAmount,
+                  opacity: 0,
+                  filter: `blur(${mediumPreset.blurAmount}px)`,
+                }
+          }
+          animate={
+            shouldReduceMotion
+              ? {}
+              : {
+                  y: 0,
+                  opacity: 1,
+                  scale: mediumPreset.scaleAmount,
+                  filter: "blur(0px)",
+                }
+          }
+          exit={
+            shouldReduceMotion
+              ? {}
+              : {
+                  y: -mediumPreset.distance,
+                  opacity: 0,
+                  scale: mediumPreset.scaleAmount,
+                  filter: `blur(${extraLargePreset.blurAmount}px)`,
+                }
+          }
+          transition={{
+            duration: shouldReduceMotion ? 0 : extraLargePreset.duration,
+            type: "spring",
+            stiffness: 200,
+            damping: 22,
+          }}
+        >
+          <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
+            <HeaderLink text="Home" to="/" />
+          </div>
+          <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
+            <HeaderLink text="Team" to="/team" />
+          </div>
+          <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
+            <HeaderLink text="Portfolio" to="/portfolio" />
+          </div>
+          <div className="flex w-full" onClick={() => setIsDropdownOpen(false)}>
+            <HeaderLink text="Contact" to="/contact" />
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </header>
   );
-}
+};;
